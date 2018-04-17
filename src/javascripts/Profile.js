06433b0e5a1dc3055cea82179aad623d019d8106
Raymond Huynh <raymond.h@myplanet.com>
2018-04-17 13:28:18 -0400

    initial commit

---
+++ src/javascripts/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default Profile = (props) => {
+  const {displayPicture, name, skills} = props;
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+
+Profile.propTypes = {
+  person: PropTypes.object
+}
