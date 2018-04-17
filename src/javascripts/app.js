90919f8dc49f7cde9ff074ab759fd9e0df2b2c57
Raymond Huynh <raymond.h@myplanet.com>
2018-04-17 13:17:09 -0400

    initial commit

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -9,16 +9,26 @@ import SearchInput from './SearchInput.js'
 
 const people = [
   {
-    name: "Rob",
-    dev: 5,
-    talk: 1,
-    ux: 2
+    displayPicture: 'https://media.licdn.com/dms/image/C4E03AQGtNUSpc-Jcjw/profile-displayphoto-shrink_800_800/0?e=1529172000&v=beta&t=hrX1a6H6qsU2f0kvufeEgp7ZiIzFybYn5HOPPO0yn1w',
+    name: 'Sudar Ganes',
+    skills: {
+      dev: [
+        {html: 5},
+        {css: 10},
+        {javaScript: 10}
+      ],
+      design: [
+        {visualDesign: 9},
+        {invision: 8},
+        {interactionDesign: 10}
+      ]
+    }
   }
 ]
 
 ReactDOM.render(
   <div>
-    <Profile people={people} person={peron} />
+    <Profile person={people[0]} />
     <SearchInput people={people} />
   </div>,
   document.getElementById('app-container')
