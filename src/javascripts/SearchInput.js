9b6fd5d995cf1d7cdca4393df7f612db9173089f
slode <rob.m@myplanet.com>
2018-04-17 14:25:04 -0400

    downgrade react to v15 to get suggestible-input working

--- src/javascripts/SearchInput.js
+++ src/javascripts/SearchInput.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import SuggestibleInput from 'suggestible-input'
+//import SuggestibleInput from '../../../tool-boxes/suggestible-input/src/suggestible-input.js'
+
+var suggestions = [
+  'Toronto',
+  'Montreal',
+  'Ottawa',
+  'Vancouver',
+  'Edmonton',
+  'Calgary'
+]
 
 const SearchInput = ({ people }) => (
   <div className="search-input">
-    <input type="text" />
+    <SuggestibleInput suggestions={suggestions} />
   </div>
 )
 
