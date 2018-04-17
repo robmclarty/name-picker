35d9dcdc0e198d0194c3612df43bc53ed47b0b9d
slode <rob.m@myplanet.com>
2018-04-17 13:27:04 -0400

    lay out basic build pipeline

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -2,9 +2,8 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 
-import Profile from './Profile.js'
+//import Profile from './Profile.js'
 import SearchInput from './SearchInput.js'
 
 const people = [
@@ -16,9 +15,28 @@ const people = [
   }
 ]
 
+const person = {
+  displayPicture: 'https://media.licdn.com/dms/image/C4E03AQGtNUSpc-Jcjw/profile-displayphoto-shrink_800_800/0?e=1529172000&v=beta&t=hrX1a6H6qsU2f0kvufeEgp7ZiIzFybYn5HOPPO0yn1w',
+  name: 'Sudar Ganes',
+  skills: {
+    dev: [
+      {html: 5},
+      {css: 10},
+      {javaScript: 10}
+    ],
+    design: [
+      {visualDesign: 9},
+      {invision: 8},
+      {interactionDesign: 10}
+    ]
+  }
+}
+
+// <Profile person={person} />
+
 ReactDOM.render(
   <div>
-    <Profile people={people} person={peron} />
+
     <SearchInput people={people} />
   </div>,
   document.getElementById('app-container')
